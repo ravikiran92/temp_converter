8ad725a9ee1196885623d36263ca5df6e0057699
Ravikiran Kota <ravikiran.k@foxberry.in>
2023-11-20 19:38:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+  }
+  
+  function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+  }
+  
+  module.exports = {
+    celsiusToFahrenheit,
+    fahrenheitToCelsius
+  };
+  
